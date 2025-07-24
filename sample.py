7223184d8e71844070ca5d883042bdcafe117398
SUDHEER AMBADIPUDI <167128289+SudheerAmbadipudi@users.noreply.github.com>
2025-07-24 18:02:57 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,12 @@
+# age eligibility
+
+age = int(input("enter your age: "))
+
+if age<= 0:
+    print("you haven't born yet :")
+elif age>=100:
+    print("you will be dead mf")
+elif age >= 18:
+    print("you are eligible to sign in :")
+else:
+    print("you are under age to sign in :")
